5d3b2b713bdc95010537e04c410757b21adbedf0
Chris Kehoe <chriskehoe@Chriss-MacBook-Air.local>
2021-12-20 12:40:24 -0800

    vents and ratings are now rendering to the vents page

--- routes/vents.js
+++ routes/vents.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const ventsCtrl = require('../controllers/vents')
 const isLoggedIn = require('../config/auth');
 
-router.post('/rents,:id/vents', isLoggedIn, ventsCtrl.create)
-router.
+router.post('/rents/:id/vents', isLoggedIn, ventsCtrl.create)
+
 module.exports = router;
